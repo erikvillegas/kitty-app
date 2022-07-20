6ba694066e3d74baec0c596a438c80a5ce1f73e9
Erik Villegas <erik.villegas@carvana.com>
2022-07-19 20:17:41 -0700

    timezones suck

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import datetime
+import pytz
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
@@ -48,7 +49,8 @@ def submit():
         return "Please enter at least one weight!"
 
     # Submit elements to Google Sheets
-    timestamp = datetime.datetime.now().strftime("%-m/%d %-I:%M %p")    
+    now = datetime.datetime.now(pytz.timezone('US/Pacific'))   
+    timestamp = now.strftime("%-m/%d %-I:%M %p") 
     insertRow = [timestamp, red, orange, yellow, green, blue, purple]
     weights.append_row(insertRow, value_input_option='USER_ENTERED')
 
