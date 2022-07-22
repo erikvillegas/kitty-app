ae76c70923b2317f475a6cd02aa34ba99d7c3fe9
Erik Villegas <erik.villegas@carvana.com>
2022-07-21 19:21:42 -0700

    kitty profiles w/ weight calculations

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+from unicodedata import decimal
 from flask import Flask, render_template, request
 import datetime
 import pytz
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
+import math
 
 app = Flask(__name__)
 
@@ -55,3 +57,106 @@ def submit():
     weights.append_row(insertRow, value_input_option='USER_ENTERED')
 
     return render_template("submit.html")
+
+@app.route('/profiles')
+def profiles():
+
+    # use worksheet to calculate everything
+    chart_data = sheet.get_worksheet(1)
+    time_strings = chart_data.col_values(1)
+    time_codes = chart_data.col_values(2)
+    red_weights = chart_data.col_values(5)
+    orange_weights = chart_data.col_values(10)
+    yellow_weights = chart_data.col_values(14)
+    green_weights = chart_data.col_values(18)
+    blue_weights = chart_data.col_values(22)
+    purple_weights = chart_data.col_values(26)
+
+    kitten_strings = []
+
+    (w, t) = calculate(time_codes, red_weights, 1.0)
+    kitten_strings.append(kitten_string(w, t, 'Red'))
+
+    (w, t) = calculate(time_codes, red_weights, 0.5)
+    kitten_strings.append(kitten_string(w, t, 'Red'))
+
+    (w, t) = calculate(time_codes, orange_weights, 1.0)
+    kitten_strings.append(kitten_string(w, t, 'Orange'))
+
+    (w, t) = calculate(time_codes, orange_weights, 0.5)
+    kitten_strings.append(kitten_string(w, t, 'Orange'))
+
+    (w, t) = calculate(time_codes, yellow_weights, 1.0)
+    kitten_strings.append(kitten_string(w, t, 'Yellow'))
+
+    (w, t) = calculate(time_codes, yellow_weights, 0.5)
+    kitten_strings.append(kitten_string(w, t, 'Yellow'))
+
+    (w, t) = calculate(time_codes, green_weights, 1.0)
+    kitten_strings.append(kitten_string(w, t, 'Green'))
+
+    (w, t) = calculate(time_codes, green_weights, 0.5)
+    kitten_strings.append(kitten_string(w, t, 'Green'))
+
+    (w, t) = calculate(time_codes, blue_weights, 1.0)
+    kitten_strings.append(kitten_string(w, t, 'Blue'))
+
+    (w, t) = calculate(time_codes, blue_weights, 0.5)
+    kitten_strings.append(kitten_string(w, t, 'Blue'))
+
+    (w, t) = calculate(time_codes, purple_weights, 1.0)
+    kitten_strings.append(kitten_string(w, t, 'Purple'))
+
+    (w, t) = calculate(time_codes, purple_weights, 0.5)
+    kitten_strings.append(kitten_string(w, t, 'Purple'))
+
+    return render_template("profiles.html", strings=kitten_strings)
+
+def is_float(element: str):
+    try:
+        float(element)
+        return True
+    except ValueError:
+        return False
+
+def kitten_string(weight_difference, time_difference, kitten):
+    gain_loss = 'gained' if weight_difference >= 0 else 'lost'
+    return f'{kitten} has {gain_loss} {weight_difference} grams in the last {math.trunc(time_difference)} hours'
+
+def calculate(times, weights: 'list[str]', time_diff):
+
+    # clean values
+    times = list(map(lambda time:  float(time) if (is_float(time)) else 0, times))
+    weights = list(map(lambda weight:  int(weight) if (weight.isnumeric()) else 0, weights))
+
+    last_weight = 0
+    last_weight_idx = 0
+    for index, value in enumerate(weights):
+        last_weight = value
+        last_weight_idx = index
+
+    last_weight_code = times[last_weight_idx]
+
+    target_time = last_weight_code - time_diff 
+    time_code_differences = list(map(lambda code: code - target_time, times))
+
+    matched_time_code = 1000
+    matched_time_code_idx = 0
+    for index, value in enumerate(time_code_differences):
+        weight = weights[index]
+
+        if value < abs(matched_time_code) and weight != 0:
+            matched_time_code = value
+            matched_time_code_idx = index
+            # print(f'closer time code found: {times[matched_time_code_idx]} ({matched_time_code}) -- weight: {weight}')
+
+    # print(f'closest time code: {times[matched_time_code_idx]}')
+    # print(f'matching weight: {weights[matched_time_code_idx]}')
+
+    comparison_weight = weights[matched_time_code_idx]
+    comparison_weight_code = times[matched_time_code_idx]
+
+    weight_difference = last_weight - comparison_weight
+    hour_difference = (last_weight_code - comparison_weight_code) * 24
+
+    return (weight_difference, hour_difference)
