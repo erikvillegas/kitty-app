3e1c5bca886891eb242a91830bc015a38de42975
Erik Villegas <erik.villegas@carvana.com>
2022-07-28 10:07:27 -0700

    chart embed and better data

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from unicodedata import decimal
 from flask import Flask, render_template, request, url_for
 import datetime
+from datetime import timedelta
 import pytz
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 import math
 import json
+import statistics
+from typing import Iterable 
 
 app = Flask(__name__)
 
@@ -25,11 +28,15 @@ google_creds = {
   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/kitten-tracker%40kittens-356816.iam.gserviceaccount.com"
 }
 
+
+
 scope = ['https://www.googleapis.com/auth/spreadsheets']
 creds = ServiceAccountCredentials.from_json_keyfile_dict(google_creds, scopes=scope)
 client = gspread.authorize(creds)
 sheet = client.open_by_key('1XMPMgAW98UtbGX7EI0ovv8HQV3-cnXUaMB-xof9AgfI')
 weights = sheet.get_worksheet(0)
+
+scores = {}
 
 ### Routes ###
 
@@ -75,7 +82,6 @@ def profiles():
 
     # use worksheet to calculate everything
     chart_data = sheet.get_worksheet(1)
-    time_strings = chart_data.col_values(1)
     time_codes = chart_data.col_values(2)
     red_weights = chart_data.col_values(5)
     orange_weights = chart_data.col_values(10)
@@ -84,45 +90,77 @@ def profiles():
     blue_weights = chart_data.col_values(22)
     purple_weights = chart_data.col_values(26)
 
-    kitten_strings = []
+    red_gains_24 = calculate_gains(time_codes, red_weights, 1.0)
+    red_gains_12 = calculate_gains(time_codes, red_weights, 0.5)
 
-    (w, t) = calculate(time_codes, red_weights, 1.0)
-    kitten_strings.append(kitten_string(w, t, 'Red'))
+    red_strings = list(flatten([
+        format_weight(red_gains_24),
+        format_weight(red_gains_12),
+        average_gains(time_codes, red_weights)
+    ]))
 
-    (w, t) = calculate(time_codes, red_weights, 0.5)
-    kitten_strings.append(kitten_string(w, t, 'Red'))
+    orange_gains_24 = calculate_gains(time_codes, orange_weights, 1.0)
+    orange_gains_12 = calculate_gains(time_codes, orange_weights, 0.5)
 
-    (w, t) = calculate(time_codes, orange_weights, 1.0)
-    kitten_strings.append(kitten_string(w, t, 'Orange'))
+    orange_strings = list(flatten([
+        format_weight(orange_gains_24),
+        format_weight(orange_gains_12),
+        average_gains(time_codes, orange_weights)
+    ]))
 
-    (w, t) = calculate(time_codes, orange_weights, 0.5)
-    kitten_strings.append(kitten_string(w, t, 'Orange'))
+    yellow_gains_24 = calculate_gains(time_codes, yellow_weights, 1.0)
+    yellow_gains_12 = calculate_gains(time_codes, yellow_weights, 0.5)
 
-    (w, t) = calculate(time_codes, yellow_weights, 1.0)
-    kitten_strings.append(kitten_string(w, t, 'Yellow'))
+    yellow_strings = list(flatten([
+        format_weight(yellow_gains_24),
+        format_weight(yellow_gains_12),
+        average_gains(time_codes, yellow_weights)
+    ]))
 
-    (w, t) = calculate(time_codes, yellow_weights, 0.5)
-    kitten_strings.append(kitten_string(w, t, 'Yellow'))
+    green_gains_24 = calculate_gains(time_codes, green_weights, 1.0)
+    green_gains_12 = calculate_gains(time_codes, green_weights, 0.5)
 
-    (w, t) = calculate(time_codes, green_weights, 1.0)
-    kitten_strings.append(kitten_string(w, t, 'Green'))
+    green_strings = list(flatten([
+        format_weight(green_gains_24),
+        format_weight(green_gains_12),
+        average_gains(time_codes, green_weights)
+    ]))
 
-    (w, t) = calculate(time_codes, green_weights, 0.5)
-    kitten_strings.append(kitten_string(w, t, 'Green'))
+    blue_gains_24 = calculate_gains(time_codes, blue_weights, 1.0)
+    blue_gains_12 = calculate_gains(time_codes, blue_weights, 0.5)
 
-    (w, t) = calculate(time_codes, blue_weights, 1.0)
-    kitten_strings.append(kitten_string(w, t, 'Blue'))
+    blue_strings = list(flatten([
+        format_weight(blue_gains_24),
+        format_weight(blue_gains_12),
+        average_gains(time_codes, blue_weights)
+    ]))
 
-    (w, t) = calculate(time_codes, blue_weights, 0.5)
-    kitten_strings.append(kitten_string(w, t, 'Blue'))
+    purple_gains_24 = calculate_gains(time_codes, purple_weights, 1.0)
+    purple_gains_12 = calculate_gains(time_codes, purple_weights, 0.5)
 
-    (w, t) = calculate(time_codes, purple_weights, 1.0)
-    kitten_strings.append(kitten_string(w, t, 'Purple'))
+    purple_strings = list(flatten([
+        format_weight(purple_gains_24),
+        format_weight(purple_gains_12),
+        average_gains(time_codes, purple_weights)
+    ]))
 
-    (w, t) = calculate(time_codes, purple_weights, 0.5)
-    kitten_strings.append(kitten_string(w, t, 'Purple'))
+    scores = {
+        'red': emoji_score(red_gains_24[0]),
+        'orange': emoji_score(orange_gains_24[0]),
+        'yellow': emoji_score(yellow_gains_24[0]),
+        'green': emoji_score(green_gains_24[0]),
+        'blue': emoji_score(blue_gains_24[0]),
+        'purple': emoji_score(purple_gains_24[0])
+    }
 
-    return render_template("profiles.html", strings=kitten_strings)
+    return render_template("profiles.html", 
+        red=red_strings, 
+        orange=orange_strings, 
+        yellow=yellow_strings, 
+        green=green_strings, 
+        blue=blue_strings, 
+        purple=purple_strings,
+        scores=scores)
 
 def is_float(element: str):
     try:
@@ -131,12 +169,25 @@ def is_float(element: str):
     except ValueError:
         return False
 
-def kitten_string(weight_difference, time_difference, kitten):
-    gain_loss = 'gained' if weight_difference >= 0 else 'lost'
-    grams = 'grams' if abs(weight_difference) > 1 else 'gram'
-    return f'{kitten} has {gain_loss} {abs(weight_difference)} {grams} in the last {math.trunc(time_difference)} hours'
+def emoji_score(gain):
+    if gain >= 14:
+        return '😎'
+    elif gain >= 12:
+        return '🙂'
+    elif gain >= 10:
+        return '🤔'
+    elif gain >= 6:
+        return '😟'
+    else:
+        return '🚨'
 
-def calculate(times, weights: 'list[str]', time_diff):
+def format_weight(tuple):
+    weight_difference, time_difference = tuple
+    gain_loss = 'Gained' if weight_difference >= 0 else 'Lost'
+    grams = 'grams' if abs(weight_difference) > 1 else 'gram'
+    return f'{gain_loss} {abs(weight_difference)} {grams} since weigh-in {math.trunc(time_difference)} hours ago'
+
+def calculate_gains(times, weights: 'list[str]', time_diff):
 
     # clean values
     times = list(map(lambda time:  float(time) if (is_float(time)) else 0, times))
@@ -176,3 +227,82 @@ def calculate(times, weights: 'list[str]', time_diff):
     hour_difference = (last_weight_code - comparison_weight_code) * 24
 
     return (weight_difference, hour_difference)
+
+
+def average_gains(times, weights):
+    # clean values
+    times = list(map(lambda time:  float(time) if (is_float(time)) else 0, times))
+    weights = list(map(lambda weight:  int(weight) if (weight.isnumeric()) else 0, weights))
+
+    weights_start_idx = next(i for i,v in enumerate(weights) if int(v) > 0)
+    times = times[weights_start_idx : len(weights)]
+    weights = weights[weights_start_idx :]
+
+    # hack to get the last day to be read too
+    times.append(1000)
+
+    averages = []
+
+    start_idx = 0
+    current_day = math.trunc(times[0])
+    for idx, value in enumerate(times):
+        if value == 0: continue
+
+        if math.trunc(value) != current_day and idx != 0:
+
+            day_weights = list(filter(lambda w: w > 0, weights[start_idx : idx]))
+            avg = statistics.fmean(day_weights)
+
+            averages.append((current_day, round(avg, 2)))
+
+            current_day = math.trunc(value)
+            start_idx = idx
+
+    # previous = averages[0][1]
+    # for day, weight in averages[1:]:
+    #     print(f'On day {day} gain was {round(weight - previous)} grams')
+    #     previous = weight
+
+    # def gain(averages, i, j):
+    #     difference = averages[i][1] - averages[j][1]
+    #     symbol = '+' if difference > 0 else '-'
+    #     #return f'{symbol}{round(difference)} grams'
+    #     return difference
+
+    strings = []
+
+    current_gain = averages[-1][1] - averages[-2][1]
+    strings.append(f'Today: gained {round(current_gain, 1)} grams from yesterday\'s average')
+
+    previous_gain = averages[-2][1] - averages[-3][1]
+    previous_day = (datetime.datetime.now() - timedelta(2)).strftime('%A')
+    strings.append(f'Yesterday: Gained {round(previous_gain, 1)} grams from {previous_day}\'s average')
+
+    previous_gain = averages[-3][1] - averages[-4][1]
+    previous_previous_day = (datetime.datetime.now() - timedelta(3)).strftime('%A')
+    strings.append(f'{previous_day}: Gained {round(previous_gain, 1)} grams from {previous_previous_day}\'s average')
+    
+    window = [
+        averages[-1][1] - averages[-2][1],
+        averages[-2][1] - averages[-3][1],
+        averages[-3][1] - averages[-4][1],
+        averages[-4][1] - averages[-5][1]
+    ]
+    window_avg = statistics.fmean(window)
+
+    return strings
+
+    # (w, t) = calculate(time_codes, raw_weights, 3.0)
+    # snapshot_difference = w/3.0
+
+    # print(f'72-hour jump on average is {snapshot_difference}')
+    # print(f'Final 72-hour averages is {snapshot_difference + }')
+
+def flatten(items):
+    """Yield items from any nested iterable; see Reference."""
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
+            for sub_x in flatten(x):
+                yield sub_x
+        else:
+            yield x
